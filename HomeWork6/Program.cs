42455439b8cf34cd5e6d0ff918c03a9f056d7569
Антон Кулакевич <antonkulakevich@mail.ru>
2022-07-24 21:05:45 +0300

    Добавьте файлы проекта.

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Text;
+
+namespace HomeWork6
+{
+    internal class Program
+    {
+        #region Запись в файл
+        public static void CreateAndWriteFile()
+        {
+
+            using (StreamWriter sw = new StreamWriter("DB.csv", true, Encoding.Unicode))
+            {
+                string worker = string.Empty;
+
+                int id;
+                Random rand = new Random();
+                id = rand.Next(1000);
+                string ID = Convert.ToString(id);           //Рандомное ID
+                worker += $"{ID}#\t";
+
+                string time = DateTime.Now.ToString();  //Время создания
+                worker += $"{time}#\t";
+
+                Console.WriteLine("Введите ФИО");
+                worker += $"{Console.ReadLine()}#\t";
+
+                Console.WriteLine("Введите возраст");
+                worker += $"{Console.ReadLine()}#\t";
+
+                Console.WriteLine("Введите рост");
+                worker += $"{Console.ReadLine()}#\t";
+
+                Console.WriteLine("Введите дату рождения, в формате dd.mm.yyyy");
+                worker += $"{Console.ReadLine()}#\t";
+
+                Console.WriteLine("Введите место рождения");
+                worker += $"{Console.ReadLine()}#\t";
+                sw.WriteLine(worker);
+            }
+            Console.Clear();
+            Console.WriteLine("Продолжить? y/n");
+
+            char flag = char.Parse(Console.ReadLine());
+            switch (flag)
+            {
+                case 'y':
+                    Main();
+                    break;
+                case 'n':
+                    break;
+            }
+        }
+        #endregion
+        #region Чтение с файла
+        public static void ReadFile()
+        {
+            using (StreamReader sr = new StreamReader("DB.csv", Encoding.Unicode))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split('#');
+
+                    Console.WriteLine($"{data[0]}\t{data[1]}\t{data[2]}\t{data[3]}\t{data[4]}\t{data[5]}\t{data[6]}\t");
+                }
+
+
+            }
+            #endregion
+            Console.WriteLine("Продолжить? y/n");
+
+            char flag = char.Parse(Console.ReadLine());
+            switch (flag)
+            {
+                case 'y':
+                    Main();
+                    Console.Clear();
+                    break;
+                case 'n':
+                    Console.Clear();
+                    break;
+            }
+        }
+
+        static void Main()
+        {
+
+            bool flag = File.Exists("DB.csv");
+            if (flag == false)
+            {
+                Console.WriteLine($"Файл существует - {flag}");
+                using (StreamWriter sw = new StreamWriter("DB.csv", true, Encoding.Unicode))
+                {
+                    string prepText = "ID#\tВремя#\tФИО#\tВозраст#\tРост#\tДата рождения#\tМесто рождения#";
+                    sw.WriteLine(prepText);
+                } 
+            }
+            else
+            {
+                Console.WriteLine($"Файл существует - {flag}");
+            }
+            Console.WriteLine("Введите 1, чтобы записать\tВведите 2, чтобы прочитать\t Введите 3, чтобы удалить");
+            int writeOrRead = int.Parse(Console.ReadLine());
+            switch (writeOrRead)
+            {
+                case 1:
+                    CreateAndWriteFile();
+                    Console.Clear();
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    ReadFile();
+                    break;
+
+                case 3:
+                    File.Delete("DB.csv");
+                    Console.Clear();
+                    Main();
+                    break;
+            }
+        }
+    }
+}
---
+++ HomeWork6/Worker.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    struct Worker
+    {
+        #region Поля Worker
+        /// <summary>
+        /// ID Сотрудника
+        /// </summary>
+        public int ID;
+
+        /// <summary>
+        /// Время добавления
+        /// </summary>
+        public DateTime DateTime;
+
+        /// <summary>
+        /// ФИО сотрудника
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public uint Age;
+
+        /// <summary>
+        /// Рост
+        /// </summary>
+        public uint Height;
+
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime Birthday;
+
+        /// <summary>
+        /// Место рождения
+        /// </summary>
+        public string BirthLocate;
+        #endregion
+
+        #region Конструктор Worker
+        public Worker(int id, DateTime dateTime, string name, uint age, uint height, DateTime birthday, string birthLocate )
+        {
+            this.ID = id;
+            this.DateTime = dateTime;
+            this.Name = name;
+            this.Age = age;
+            this.Height = height;
+            this.Birthday = birthday;
+            this.BirthLocate = birthLocate;
+        }
+        #endregion
+
+        #region Методы
+
+        public void CreateNote()
+        {
+            Console.WriteLine("Введите имя файла - ");
+            string fileName = Console.ReadLine();
+            using (StreamWriter wr = new StreamWriter($"{fileName}.csv", true, Encoding.Unicode))
+            {
+                Random r = new Random();
+                ID = r.Next(1000);
+
+                DateTime = DateTime.Now;
+
+                Console.WriteLine("Введите имя сотрудника - ");
+                Name = Console.ReadLine();
+
+                Console.WriteLine("Введите возраст сотрудника - ");
+                Age = uint.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите рост сотрудника - ");
+                Height = uint.Parse(Console.ReadLine());
+
+                Birthday = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите место рождения - ");
+                BirthLocate = Console.ReadLine();
+
+                wr.WriteLine();
+
+            }
+        }
+
+        #endregion
+    }
+    
+}
